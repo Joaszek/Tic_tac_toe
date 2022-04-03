186d3a4c31d51bae130522e0fd0c974cdfb6935a
Joaszek <joachim.szewior@gmail.com>
2022-04-03 22:40:18 +0200

    new update, before mouse method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
+import java.awt.*;
+
 public class Main {
     public static void main(String []Args)
     {
         myFrame frame = new myFrame();
-
+        frame.setBackground(Color.BLACK);
     }
 
 }
--- src/main/java/myFrame.java
+++ src/main/java/myFrame.java
@@ -1,64 +1,41 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class myFrame extends JFrame {
-    private final ImageIcon cros;
-    private final ImageIcon circle;
-    private final JLabel label1;
-    private final JLabel label2;
+import static java.awt.Color.BLACK;
+import static java.awt.Color.getColor;
+
+public class myFrame extends JFrame implements ActionListener {
+
+
     myFrame()
     {
-        cros = new ImageIcon("cross.png");
-        circle = new ImageIcon("circle.png");
 
-        label1 = new JLabel();
-        label1.setIcon(circle);
-        label1.setSize(300,300);
-
-        label2 = new JLabel();
-        label2.setIcon(cros);
-        label2.setSize(300,300);
-
-
-        BufferedImage img = null;
-        try {
-            img = ImageIO.read(new File("circle.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Image dimg = img.getScaledInstance(label1.getWidth(), label1.getHeight(),Image.SCALE_SMOOTH);
-
-        BufferedImage img2 = null;
-        try {
-            img = ImageIO.read(new File("cross.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Image dimg2 = img.getScaledInstance(label2.getWidth(), label2.getHeight(),Image.SCALE_SMOOTH);
-
-
-        ImageIcon imageCircle = new ImageIcon(dimg);
-        ImageIcon imageCross = new ImageIcon(dimg2);
-        JLabel []labels= new JLabel[9];
+       JLabel []labels= new JLabel[9];
         for(int i=0;i<9;i++) {
             labels[i] = new JLabel();
-            labels[i].setOpaque(true);
-            labels[i].setBackground(Color.black);
+
             this.add(labels[i]);
         }
+        JPanel panel = new JPanel();
 
-
-
-
-        this.setBackground(Color.black);
+        this.setTitle("TIC TAC TOE");
+        this.getContentPane().setBackground(BLACK);
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
-        this.setSize(3*imageCross.getIconWidth(),3*imageCross.getIconHeight());
+        this.setSize((int) (3*Pool.get_image_Cross_length()),(int)(3*Pool.get_image_Cross_width()));
         this.setVisible(true);
+    }
+    void set_label(){}
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //mouse click method
     }
 }
