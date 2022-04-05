f467b2a926caad081dfa3521133f9d6c694e1acc
Joaszek <joachim.szewior@gmail.com>
2022-04-05 13:18:38 +0200

    merge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String []Args)
     {
         myFrame frame = new myFrame();
+        //nowy komentarz
         frame.setBackground(Color.BLACK);
         //nowy branch do proby
     }
